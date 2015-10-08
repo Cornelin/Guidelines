7c094888ad9e0eff6c5fc7b57d35764626ac50c8
Cornik34 <lancelot.prigent@gmail.com>
2015-10-09 00:25:04 +0200

    comment with keywords

--- es5/pattern.js
+++ es5/pattern.js
@@ -302,6 +302,28 @@ function getType() {
   return type;
 }
 
+// Prefixing your comments with FIXME or TODO helps other developers quickly understand if you're pointing out a problem that needs to be revisited, or if you're suggesting a solution to the problem that needs to be implemented. These are different than regular comments because they are actionable. The actions are FIXME -- need to figure this out or TODO -- need to implement.
+
+// Use // FIXME: to annotate problems.
+
+function Calculator() {
+
+  // FIXME: shouldn't use a global here
+  total = 0;
+
+  return this;
+}
+
+// Use // TODO: to annotate solutions to problems.
+
+function Calculator() {
+
+  // TODO: total should be configurable by an options param
+  this.total = 0;
+
+  return this;
+}
+
 // Final returns should be preceded by a blank line
 
 // bad
